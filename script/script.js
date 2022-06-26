2dedb1936842f68bef6c7ff5fe4b7a5f98c358ff
Rodrigo Medeiros <medeiros11.dev@gmail.com>
2022-06-25 23:24:40 -0300

    JS

--- script/script.js
+++ script/script.js
@@ -21,5 +21,4 @@ form.addEventListener('submit', (e)=>{
 function isValidEmail( value ) {
 	return /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,5}$/.test( value );
 }
-
 console.log( isValidEmail("mymail@mydomain.com") )
